bd994c2abe11defb0c072f78598d76e955676d4c
nilsmanc <etereaiberia@gmail.com>
2023-07-05 08:11:38 +0300

    login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,10 @@
-import { Card, Form, Input, Row } from 'antd'
+import { Card, Form, Row, Space, Typography } from 'antd'
 import { Layout } from '../../components/layout'
+import { CustomInput } from '../../components/custom-input'
+import { PasswordInput } from '../../components/password-input'
+import { CustomButton } from '../../components/custom-button'
+import { Link } from 'react-router-dom'
+import { Paths } from '../../paths'
 
 export const Login = () => {
   return (
@@ -7,8 +12,17 @@ export const Login = () => {
       <Row align="middle" justify="center">
         <Card title="Sign in" style={{ width: '30rem' }}>
           <Form onFinish={() => null}>
-            <Input />
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Password" />
+            <CustomButton type="primary" htmlType="submit">
+              Sign in
+            </CustomButton>
           </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Don't have account? <Link to={Paths.register}>Sign up</Link>
+            </Typography.Text>
+          </Space>
         </Card>
       </Row>
     </Layout>
