6f31ba27f1781f8d18651f2409e60c1fe1c3fea5
nilsmanc <etereaiberia@gmail.com>
2023-07-18 07:42:55 +0300

    employees slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -5,12 +5,14 @@ import {
   getDefaultMiddleware,
 } from '@reduxjs/toolkit'
 import auth from '../features/auth/authSlice'
+import employees from '../features/employees/employeesSlice'
 import { api } from './services/api'
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth,
+    employees,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
