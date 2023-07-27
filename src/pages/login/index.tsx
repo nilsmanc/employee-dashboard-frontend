687f4a27593a925b8bfad6b2d7515f830440182c
nilsmanc <etereaiberia@gmail.com>
2023-07-27 08:19:03 +0300

    imports

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,7 +9,6 @@ import { CustomButton } from '../../components/custom-button'
 import { Paths } from '../../paths'
 import { UserData, useLoginMutation } from '../../app/services/auth'
 import { isErrorWithMessage } from '../../utils/is-error-with-message'
-
 import { ErrorMessage } from '../../components/error-message'
 
 export const Login = () => {
